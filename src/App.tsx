a488ca48c5ea8b8f71975f9457fedc80454ec8af
sam33r0 <srivastavasameer2002@gmail.com>
2024-06-23 19:50:30 +0530

    W/O readme and comments

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,93 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
+import TableYear from './Components/TableYear';
+import TableCrop from './Components/TableCrop';
+import '@mantine/core/styles.css';
+import TableViewer from './Components/TableViewer/TableViewer';
 
 function App() {
-  const [data, setData] = useState();
+  // const [ymap, setYmap] = useState({});
+  // const [crmap, setCrmap] = useState({});
+
+  const [data, setData]=useState([])
   useEffect(() => {
-    fetch('./Manufac _ India_Agro_Dataset.json')
-      .then(response => {
-        return response.json();
-      }).then(data => {
+    fetch('./Manufac_India_Agro_Dataset.json')
+      .then(response => response.json())
+      .then(data => {
+        // const { yearMap, cropMap } = processData(data);
+        // setYmap(yearMap);
+        // setCrmap(cropMap);
         setData(data);
-        console.log(data);
-      }).catch(err => {
-        console.log("Error Reading data " + err);
+      })
+      .catch(err => {
+        console.error("Error reading data", err);
       });
-  }, [])
+  }, []);
+
+  // const processData = (data: Array<any>) => {
+  //   let cropMap = {};
+  //   let yearMap = {};
+  //   let op = 0;
+  //   let yr = 1950;
+  //   let minProd = Number.MAX_VALUE;
+  //   let minProdCrop = "";
+  //   let maxProdCrop = "";
+  //   let maxProd = Number.MIN_VALUE;
+
+  //   data.forEach((elem, ind) => {
+  //     const index = Math.floor(ind / 13);
+
+  //     if (!cropMap[elem["Crop Name"]]) {
+  //       cropMap[elem["Crop Name"]] = { ArC: 0, YlD: 0, n: 0 };
+  //     }
+
+  //     const areaUnderCultivation = parseFloat(elem["Area Under Cultivation (UOM:Ha(Hectares))"]) || 0;
+  //     const yieldOfCrops = parseFloat(elem["Yield Of Crops (UOM:Kg/Ha(KilogramperHectare))"]) || 0;
+
+  //     cropMap[elem["Crop Name"]].ArC += areaUnderCultivation;
+  //     cropMap[elem["Crop Name"]].YlD += yieldOfCrops;
+  //     cropMap[elem["Crop Name"]].n += 1;
+
+  //     if (index !== op) {
+  //       yearMap[yr].push({ maxProd: maxProdCrop, minProd: minProdCrop });
+  //       op++;
+  //       yr++;
+  //       maxProd = Number.MIN_VALUE;
+  //       minProd = Number.MAX_VALUE;
+  //       maxProdCrop = "";
+  //       minProdCrop = "";
+  //     }
+
+  //     if (!yearMap[yr]) {
+  //       yearMap[yr] = [];
+  //     }
+
+  //     const cropProduction = parseFloat(elem["Crop Production (UOM:t(Tonnes))"]) || 0;
+  //     yearMap[yr].push({
+  //       name: elem["Crop Name"],
+  //       prod: cropProduction,
+  //       area: areaUnderCultivation,
+  //       yield: yieldOfCrops,
+  //     });
+
+  //     if (cropProduction > maxProd) {
+  //       maxProdCrop = elem["Crop Name"];
+  //       maxProd = cropProduction;
+  //     }
+
+  //     if (cropProduction < minProd) {
+  //       minProdCrop = elem["Crop Name"];
+  //       minProd = cropProduction;
+  //     }
+  //   });
+
+  //   yearMap[yr]?.push({ maxProd: maxProdCrop, minProd: minProdCrop });
+
+  //   return { yearMap, cropMap };
+  // };
+
   return (
-    <div className="App">
-      {JSON.stringify(data)}
-    </div>
+   <TableViewer data={data}/>
   );
 }
 
---
+++ src/Components/TableViewer/TableViewer.tsx
@@ -0,0 +1,103 @@
+import React, { useEffect, useState } from 'react';
+import TableCrop from '../TableCrop';
+import TableYear from '../TableYear';
+
+interface CrmapInter {
+  n: number;
+  areaUnderCult: number;
+  yieldOfCrop: number;
+}
+
+interface YmapInter {
+  name: string;
+  prod: number;
+  area: number;
+  yield: number;
+}
+
+function TableViewer({ data }: { data: Array<any> }) {
+  const [ymap, setYmap] = useState<{ [key: number]: YmapInter[] }>({});
+  const [crmap, setCrmap] = useState<{ [key: string]: CrmapInter }>({});
+
+  const processData = (data: Array<any>) => {
+    let cropMap: { [key: string]: CrmapInter } = {};
+    let yearMap: { [key: number]: YmapInter[] } = {};
+    let op = 0;
+    let yr = 1950;
+    let minProd = Number.MAX_VALUE;
+    let minProdCrop = "";
+    let maxProdCrop = "";
+    let maxProd = Number.MIN_VALUE;
+
+    data.forEach((elem, ind) => {
+      const index = Math.floor(ind / 13);
+
+      if (!cropMap[elem["Crop Name"]]) {
+        cropMap[elem["Crop Name"]] = { areaUnderCult: 0, yieldOfCrop: 0, n: 0 };
+      }
+
+      const areaUnderCultivation = parseFloat(elem["Area Under Cultivation (UOM:Ha(Hectares))"]) || 0;
+      const yieldOfCrops = parseFloat(elem["Yield Of Crops (UOM:Kg/Ha(KilogramperHectare))"]) || 0;
+
+      cropMap[elem["Crop Name"]].areaUnderCult += areaUnderCultivation;
+      cropMap[elem["Crop Name"]].yieldOfCrop += yieldOfCrops;
+      cropMap[elem["Crop Name"]].n += 1;
+
+      if (index !== op) {
+        yearMap[yr].push({ name: maxProdCrop, prod: maxProd, area: 0, yield: 0 });
+        yearMap[yr].push({ name: minProdCrop, prod: minProd, area: 0, yield: 0 });
+        op++;
+        yr++;
+        maxProd = Number.MIN_VALUE;
+        minProd = Number.MAX_VALUE;
+        maxProdCrop = "";
+        minProdCrop = "";
+      }
+
+      if (!yearMap[yr]) {
+        yearMap[yr] = [];
+      }
+
+      const cropProduction = parseFloat(elem["Crop Production (UOM:t(Tonnes))"]) || 0;
+      yearMap[yr].push({
+        name: elem["Crop Name"],
+        prod: cropProduction,
+        area: areaUnderCultivation,
+        yield: yieldOfCrops,
+      });
+
+      if (cropProduction > maxProd) {
+        maxProdCrop = elem["Crop Name"];
+        maxProd = cropProduction;
+      }
+
+      if (cropProduction < minProd) {
+        minProdCrop = elem["Crop Name"];
+        minProd = cropProduction;
+      }
+    });
+
+    yearMap[yr]?.push({ name: maxProdCrop, prod: maxProd, area: 0, yield: 0 });
+    yearMap[yr]?.push({ name: minProdCrop, prod: minProd, area: 0, yield: 0 });
+
+    return { yearMap, cropMap };
+  };
+
+  useEffect(() => {
+    const { yearMap, cropMap } = processData(data);
+    setYmap(yearMap);
+    setCrmap(cropMap);
+  }, [data]);
+
+  return (
+    <div className="Center">
+      <h2>Table 1</h2>
+      <TableYear yMap={ymap} />
+      <br />
+      <h2>Table 2</h2>
+      <TableCrop crmap={crmap} />
+    </div>
+  );
+}
+
+export default TableViewer;
